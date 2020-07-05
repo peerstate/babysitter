58de5b39a472c958516603cf60a6a0ad915935b4
Bryan Goldstein <brysgo@gmail.com>
2020-07-04 20:02:21 -0400

    configure oclif to potentially host an unsafe flag to bypass authentication

--- src/index.ts
+++ src/index.ts
@@ -55,16 +55,13 @@ type User = {
 };
 
 class PeerstateBabysitter extends Command {
-  static description = "describe the command here";
+  static description =
+    "backend service for managing peerstate sessions and keys";
 
   static flags = {
     // add --version flag to show CLI version
     version: flags.version({ char: "v" }),
     help: flags.help({ char: "h" }),
-    // flag with a value (-n, --name=VALUE)
-    name: flags.string({ char: "n", description: "name to print" }),
-    // flag with no value (-f, --force)
-    force: flags.boolean({ char: "f" }),
   };
 
   static args = [{ name: "file" }];
@@ -72,10 +69,21 @@ class PeerstateBabysitter extends Command {
   async run() {
     const { args, flags } = this.parse(PeerstateBabysitter);
 
-    const name = flags.name ?? "world";
-    this.log(`hello ${name} from ./src/index.ts`);
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`);
+    if (Object.keys(flags).some((f) => f.indexOf("unsafe") === 0)) {
+      console.log("");
+      console.log("#######################################################");
+      console.log("#######################################################");
+      console.log("###############   UNSAFE: DO NOT USE     ##############");
+      console.log("#######################################################");
+      console.log("#######################################################");
+      console.log("");
+      console.log(
+        "This feature was meant for debugging the peerstate framework without"
+      );
+      console.log("security getting in the way.");
+      console.log("");
+      console.log("If this is on in production you will be fired.");
+      console.log("");
     }
 
     //Create a passport middleware to handle User login
